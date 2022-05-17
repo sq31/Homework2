84658c679c33e819c06b033d73dbfb6e6c78bed2
OstapchukSergey <sq31@mail.ru>
2022-05-17 22:04:06 +0300

    Task 10, 15, 13 ready

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -9,29 +9,24 @@
 Console.Write("Введите число - ");
 int number = Convert.ToInt32(Console.ReadLine());
 int Digit = 0;
+int numberX = number;
 
-if (number < 100)
+if (number > 99)
 {
-    if (number > 100 && number < 1000)
+    if (number > 99 && number < 1000)
     {
         Digit = number - (number / 10) * 10;
-        Console.WriteLine($"Третья цифра от числа {number} является {Digit}.");
     }
-
-    else
+    else if (number > 999)
     {
-        while (number > 999)
+        while (number >= 1000)
         {
             number = number / 10;
-            Digit = number - (number / 10) * 10;
         }
-        Console.WriteLine($"Третья цифра от числа {number} является {Digit}.");
-
     }
-    
-
+    Digit = number - (number / 10) * 10;
+    Console.WriteLine($"Третья цифра от числа {numberX} является {Digit}.");
 }
-
 else
 {
     Console.WriteLine("Число не является трехзначным, третьей цифры нет!");
